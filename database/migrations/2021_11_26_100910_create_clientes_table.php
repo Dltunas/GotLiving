9808faec5d0b41bbf9a744c55dd4c04341f25ac7
dltunas <dltunasd@gmail.com>
2022-06-15 08:37:24 -0500

    Master, hagan pull
    Copien estas funciones y codigo, usenlas de base

---
+++ database/migrations/2021_11_26_100910_create_clientes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('idCliente')->unique();
+            $table->foreign('idCliente')->references('id')->on('usuarios')->onDelete('cascade');
+
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('institucion');
+            $table->string('correo_e')->unique();
+            $table->string('telefono');
+            $table->string('descripcion')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ database/migrations/2021_11_26_101535_create_inmuebles_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInmueblesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inmuebles', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->unsignedBigInteger('idArrendatario');
+            $table->foreign('idArrendatario')->references('id')->on('clientes')->onDelete('cascade');
+
+            $table->text('descripcion');
+            $table->text('titulo');
+            $table->integer('calificacionGlobal');
+            $table->integer('cantidadCalificaciones');
+            $table->text('direccion');
+            $table->text('tiempoRentaMax');
+            $table->text('tiempoRentaMin');
+            $table->text('precio');
+            $table->text('foto');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inmuebles');
+    }
+}
